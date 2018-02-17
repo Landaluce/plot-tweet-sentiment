f37f2553d49c2a2f8555164d08824764562bbc74
Alvaro de Landaluce <Landaluce@users.noreply.github.com>
2018-02-17 16:14:18 -0500

    Add files via upload

---
+++ api_credentials.py
@@ -0,0 +1,15 @@
+KEY1 = {"consumer_key": "XXXXX",
+        "consumer_secret": "XXXXX",
+        "access_token": "XXXXX-XXXXX",
+        "access_token_secret": "XXXXX"}
+KEY2 = {"consumer_key": "XXXXX",
+        "consumer_secret": "XXXXX",
+        "access_token": "XXXXX-XXXXX",
+        "access_token_secret": "XXXXX"}
+KEY3 = {"consumer_key": "XXXXX",
+        "consumer_secret": "XXXXX",
+        "access_token": "XXXXX-XXXXX",
+        "access_token_secret": "XXXXX"}
+TWITTER_API_KEYS = [KEY1, KEY2, KEY3]
+
+GOOGLE_API_KEY = "XXXXX"
